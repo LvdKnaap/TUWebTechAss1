f02bd457de1e451701a2c5d1b87fe55965b1ab4e
Leon van der Knaap <leonvdknaap@gmail.com>
2018-12-13 12:14:02 +0100

    niet werkende cookie

--- yahtzee/app.js
+++ yahtzee/app.js
@@ -22,6 +22,30 @@ app.use(express.static(__dirname + "/public"));
 app.get("/play", indexRouter);
 
 
+app.use(cookies(credentials.cookieSecret));
+var sessionConfiguration = {
+	// Code is slightly adjusted to avoid deprecation warnings when running the code.
+	secret: credentials.cookieSecret,
+	resave: false,
+	saveUninitialized: true,
+};
+app.use(sessions(sessionConfiguration));
+
+app.get("/countMe", function (req, res) {
+	var session = req.session;
+	if (session.views) {
+		session.views++;
+		res.send("You have been here " + session.views + " times (last visit: " + session.lastVisit + ")");
+        session.lastVisit = new Date().toLocaleDateString();
+	}
+	else {
+		session.views = 1;
+		session.lastVisit = new Date().toLocaleDateString();
+        res.send("This is your first visit!");
+	}
+});
+
+
 
 
 //TODO: move to routes/index
